62757176e913ae570ee63d43720bab703cf71529
Schmouk <ph.schmouker@yahoo.fr>
2022-01-31 16:36:41 +0100

    #38 -archive new version of class vcl:Vect2
    Implemented, yet to be tested.

--- vcl/include/utils/vect2.h
+++ vcl/include/utils/vect2.h
@@ -49,473 +49,88 @@ namespace vcl {
         typedef Vector<TScalar, 2> MyBaseType; //<! wrapper to this class inherited class naming.
         typedef Vect2<TScalar>     MyType;     //<! wrapper to this class naming.
 
-        typedef TScalar MyScalarType; //!< wrapper to TScalar
-
 
         //---   constructors   ----------------------------------------------
-        /** \brief Constructor with a filling value (defaults to 0).
+        /** \brief Empty constructor (components default to 0).
         */
-        inline Vect2<TScalar>(const TScalar value = TScalar(0))
+        inline Vect2<TScalar>()
+            : MyBaseType()
+        {}
+
+        /** \brief Constructor with a filling value.
+        */
+        template<typename T>
+        inline Vect2<TScalar>(const T value)
             : MyBaseType(value)
         {}
 
         /** \brief Copy constructor (const&).
         */
-        inline Vect2<TScalar>(const MyType& other)
-            : MyBaseType()
-        {
-            copy(other);
-        }
-
-        /** \brief Copy constructor (&).
-        */
-        inline Vect2<TScalar>(MyType& other)
-            : MyBaseType()
-        {
-            copy(other);
-        }
+        template<typename T, size_t S>
+        inline Vect2<TScalar>(const vcl::Vector<T, S>& other)
+            : MyBaseType(other)
+        {}
 
         /** \brief Copy constructor (const std::array&).
         */
-        inline Vect2<TScalar>(const MyBaseType& other)
-            : MyBaseType()
-        {
-            copy(other);
-        }
+        template<typename T, size_t S>
+        inline Vect2<TScalar>(const std::array<T, S>& arr)
+            : MyBaseType(arr)
+        {}
 
-        /** \brief Copy constructor (std::array&).
-        */
-        inline Vect2<TScalar>(MyBaseType& other)
-            : MyBaseType()
-        {
-            copy(other);
-        }
 
         /** \brief Copy constructor (const std::vector&).
         */
-        inline Vect2<TScalar>(const std::vector<MyScalarType>& vect)
-            : MyBaseType()
-        {
-            copy(vect);
-        }
-
-        /** \brief Copy constructor (std::vector&).
-        */
-        inline Vect2<TScalar>(std::vector<MyScalarType>& vect)
-            : MyBaseType()
-        {
-            copy(vect);
-        }
-
-        /** \brief Copy Constructor (const buffer).
-        */
-        inline Vect2<TScalar>(const MyScalarType buffer[MyBaseType::KSize])
-            : MyBaseType()
-        {
-            copy(buffer);
-        }
-
-        /** \brief Copy constructor (buffer).
-        */
-        inline Vect2<TScalar>(MyScalarType buffer[MyBaseType::KSize])
-            : MyBaseType()
-        {
-            copy(buffer);
-        }
+        template<typename T>
+        inline Vect2<TScalar>(const std::vector<T>& vect)
+            : MyBaseType(vect)
+        {}
 
         //---  Destructor   -------------------------------------------------
         virtual inline ~Vect2<TScalar>()
         {}
 
         //---   Components accessors   --------------------------------------
-        /** \brief component x getter */
-        inline TScalar x()
+        /** \brief component x accessors */
+        inline TScalar& x()
         {
             return (*this)[0];
         }
 
-        /** \brief component x getter (const) */
-        inline const TScalar x() const
+        inline const TScalar& x() const
         {
             return (*this)[0];
         }
 
-        /** \brief component x setter */
-        inline TScalar x(const MyScalarType new_x)
-        {
-            return (*this)[0] = TScalar(new_x);
-        }
-
-        /** \brief component y getter */
-        inline TScalar y()
+        /** \brief component y accessors */
+        inline TScalar& y()
         {
             return (*this)[1];
         }
 
-        /** \brief component y getter (const) */
-        inline const TScalar y() const
+        inline const TScalar& y() const
         {
             return (*this)[1];
         }
 
-        /** \brief component y setter */
-        inline TScalar y(const MyScalarType new_y)
-        {
-            return (*this)[1] = TScalar(new_y);
-        }
 
         //---   copy()   ----------------------------------------------------
-        /** \brief Copies a const vcl::Vector. */
-        virtual inline void copy(const MyType& other)
-        {
-            x(other.x());
-            y(other.y());
-        }
 
-        /** \brief Copies a vcl::Vector. */
-        virtual inline void copy(MyType& other)
-        {
-            x(other.x());
-            y(other.y());
-        }
-
-        /** \brief Copies a const std::array. */
-        virtual inline void copy(const MyBaseType& other)
-        {
-            x(other[0]);
-            y(other[1]);
-        }
-
-        /** \brief Copies an std::array. */
-        virtual inline void copy(MyBaseType& other)
-        {
-            x(other[0]);
-            y(other[1]);
-        }
-
-        /** \brief Copies a const std::vector. */
-        virtual inline void copy(const std::vector<MyScalarType>& other)
-        {
-            x(other[0]);
-            y(other.size() > 1 ? other[1] : TScalar(0));
-        }
-
-        /** \brief Copies a std::vector. */
-        virtual inline void copy(std::vector<MyScalarType>& other)
-        {
-            x(other[0]);
-            y(other.size() > 1 ? other[1] : TScalar(0));
-        }
-
-        /** \brief Copies a const buffer. */
-        virtual inline void copy(const MyScalarType buffer[2])
-        {
-            x(buffer[0]);
-            y(buffer[1]);
-        }
-
-        /** \brief Copies a buffer. */
-        virtual inline void copy(MyScalarType buffer[2])
-        {
-            x(buffer[0]);
-            y(buffer[1]);
-        }
 
         //---   assignment operator   ---------------------------------------
-        /** \brief assign operator (const std::array). */
-        virtual inline MyType& operator= (const std::array<MyScalarType, MyBaseType::KSize>& other)
-        {
-            copy(other);
-            return *this;
-        }
 
-        /** \brief assign operator (std::array). */
-        virtual inline MyType& operator= (std::array<MyScalarType, MyBaseType::KSize>& other)
-        {
-            copy(other);
-            return *this;
-        }
-
+        
     protected:
         //---   add()   -----------------------------------------------------
-        /** \brief inplace add operation (const reference) */
-        virtual inline void add(const MyType& rhs)
-        {
-            x(x() + rhs.x());
-            y(y() + rhs.y());
-        }
 
-        /** \brief inplace add operation (reference) */
-        virtual inline void add(MyType& rhs)
-        {
-            x(x() + rhs.x());
-            y(y() + rhs.y());
-        }
-
-        /** \brief inplace add operation (reference) */
-        virtual inline void add(const MyScalarType value)
-        {
-            x(x() + value);
-            y(y() + value);
-        }
-
-        /** \brief inplace add operation (const std::array) */
-        virtual inline void add(const MyBaseType& rhs)
-        {
-            x(x() + rhs[0]);
-            y(y() + rhs[1]);
-        }
-
-        /** \brief inplace add operation (std::array) */
-        virtual inline void add(MyBaseType& rhs)
-        {
-            x(x() + rhs[0]);
-            y(y() + rhs[1]);
-        }
-
-        /** \brief inplace add operation (const std::vector) */
-        virtual inline void add(const std::vector<MyScalarType>& rhs)
-        {
-            x(x() + rhs[0]);
-            if (rhs.size() > 1)
-                y(y() + rhs[1]);
-        }
-
-        /** \brief inplace add operation (std::vector) */
-        virtual inline void add(std::vector<MyScalarType>& rhs)
-        {
-            x(x() + rhs[0]);
-            if (rhs.size() > 1)
-                y(y() + rhs[1]);
-        }
-
-        /** \brief inplace add operation (const buffer) */
-        virtual inline void add(const MyScalarType buffer[MyBaseType::KSize])
-        {
-            x(x() + buffer[0]);
-            y(y() + buffer[1]);
-        }
-
-        /** \brief inplace add operation (buffer) */
-        virtual inline void add(MyScalarType buffer[MyBaseType::KSize])
-        {
-            x(x() + buffer[0]);
-            y(y() + buffer[1]);
-        }
 
         //---   sub()   -----------------------------------------------------
-        /** \brief inplace sub operation (const reference) */
-        virtual inline void sub(const MyType& rhs)
-        {
-            x(x() - rhs.x());
-            y(y() - rhs.y());
-        }
 
-        /** \brief inplace sub operation (reference) */
-        virtual inline void sub(MyType& rhs)
-        {
-            x(x() - rhs.x());
-            y(y() - rhs.y());
-        }
-
-        /** \brief inplace sub operation (reference) */
-        virtual inline void sub(const MyScalarType value)
-        {
-            x(x() - value);
-            y(y() - value);
-        }
-
-        /** \brief inplace sub operation (const std::array) */
-        virtual inline void sub(const MyBaseType& rhs)
-        {
-            x(x() - rhs[0]);
-            y(y() - rhs[1]);
-        }
-
-        /** \brief inplace sub operation (std::array) */
-        virtual inline void sub(MyBaseType& rhs)
-        {
-            x(x() - rhs[0]);
-            y(y() - rhs[1]);
-        }
-
-        /** \brief inplace sub operation (const std::vector) */
-        virtual inline void sub(const std::vector<MyScalarType>& rhs)
-        {
-            x(x() - rhs[0]);
-            if (rhs.size() > 1)
-                y(y() - rhs[1]);
-        }
-
-        /** \brief inplace sub operation (std::vector) */
-        virtual inline void sub(std::vector<MyScalarType>& rhs)
-        {
-            x(x() - rhs[0]);
-            if (rhs.size() > 1)
-                y(y() - rhs[1]);
-        }
-
-        /** \brief inplace sub operation (const buffer) */
-        virtual inline void sub(const MyScalarType buffer[MyBaseType::KSize])
-        {
-            x(x() - buffer[0]);
-            y(y() - buffer[1]);
-        }
-
-        /** \brief inplace sub operation (buffer) */
-        virtual inline void sub(MyScalarType buffer[MyBaseType::KSize])
-        {
-            x(x() - buffer[0]);
-            y(y() - buffer[1]);
-        }
 
         //---   mul()   -----------------------------------------------------
-        /** \brief inplace mul operation (const reference) */
-        virtual inline void mul(const MyType& rhs)
-        {
-            x(x() * rhs.x());
-            y(y() * rhs.y());
-        }
 
-        /** \brief inplace mul operation (reference) */
-        virtual inline void mul(MyType& rhs)
-        {
-            x(x() * rhs.x());
-            y(y() * rhs.y());
-        }
-
-        /** \brief inplace mul operation (reference) */
-        virtual inline void mul(const MyScalarType value)
-        {
-            x(x() * value);
-            y(y() * value);
-        }
-
-        /** \brief inplace mul operation (const std::array) */
-        virtual inline void mul(const MyBaseType& rhs)
-        {
-            x(x() * rhs[0]);
-            y(y() * rhs[1]);
-        }
-
-        /** \brief inplace mul operation (std::array) */
-        virtual inline void mul(MyBaseType& rhs)
-        {
-            x(x() * rhs[0]);
-            y(y() * rhs[1]);
-        }
-
-        /** \brief inplace mul operation (const std::vector) */
-        virtual inline void mul(const std::vector<MyScalarType>& rhs)
-        {
-            x(x() * rhs[0]);
-            if (rhs.size() > 1)
-                y(y() * rhs[1]);
-        }
-
-        /** \brief inplace mul operation (std::vector) */
-        virtual inline void mul(std::vector<MyScalarType>& rhs)
-        {
-            x(x() * rhs[0]);
-            if (rhs.size() > 1)
-                y(y() * rhs[1]);
-        }
-
-        /** \brief inplace mul operation (const buffer) */
-        virtual inline void mul(const MyScalarType buffer[MyBaseType::KSize])
-        {
-            x(x() * buffer[0]);
-            y(y() * buffer[1]);
-        }
-
-        /** \brief inplace mul operation (buffer) */
-        virtual inline void mul(MyScalarType buffer[MyBaseType::KSize])
-        {
-            x(x() * buffer[0]);
-            y(y() * buffer[1]);
-        }
 
         //---   div()   -----------------------------------------------------
-        /** \brief inplace div operation (const reference) */
-        virtual inline void div(const MyType& rhs)
-        {
-            if (rhs.x() != TScalar(0))
-                x(x() / rhs.x());
-            if (rhs.y() != TScalar(0))
-                y(y() / rhs.y());
-        }
-
-        /** \brief inplace div operation (reference) */
-        virtual inline void div(MyType& rhs)
-        {
-            if (rhs.x() != TScalar(0))
-                x(x() / rhs.x());
-            if (rhs.y() != TScalar(0))
-                y(y() / rhs.y());
-        }
-
-        /** \brief inplace div operation (reference) */
-        virtual inline void div(const MyScalarType value)
-        {
-            if (value != TScalar(0)) {
-                x(x() / value);
-                y(y() / value);
-            }
-        }
-
-        /** \brief inplace div operation (const std::array) */
-        virtual inline void div(const MyBaseType& rhs)
-        {
-            if (rhs[0] != TScalar(0))
-                x(x() / rhs[0]);
-            if (rhs[1] != TScalar(0))
-                y(y() / rhs[1]);
-        }
-
-        /** \brief inplace div operation (std::array) */
-        virtual inline void div(MyBaseType& rhs)
-        {
-            if (rhs[0] != TScalar(0))
-                x(x() / rhs[0]);
-            if (rhs[1] != TScalar(0))
-                y(y() / rhs[1]);
-        }
-
-        /** \brief inplace div operation (const std::vector) */
-        virtual inline void div(const std::vector<MyScalarType>& rhs)
-        {
-            if (rhs[0] != TScalar(0))
-                x(x() / rhs[0]);
-            if (rhs.size() > 1  &&  rhs[1] != TScalar(0))
-                y(y() / rhs[1]);
-        }
-
-        /** \brief inplace div operation (std::vector) */
-        virtual inline void div(std::vector<MyScalarType>& rhs)
-        {
-            if (rhs[0] != TScalar(0))
-                x(x() / rhs[0]);
-            if (rhs.size() > 1 && rhs[1] != TScalar(0))
-                y(y() / rhs[1]);
-        }
-
-        /** \brief inplace div operation (const buffer) */
-        virtual inline void div(const MyScalarType buffer[MyBaseType::KSize])
-        {
-            if (buffer[0] != TScalar(0))
-                x(x() / buffer[0]);
-            if (buffer[1] != TScalar(0))
-                y(y() / buffer[1]);
-        }
-
-        /** \brief inplace div operation (buffer) */
-        virtual inline void div(MyScalarType buffer[MyBaseType::KSize])
-        {
-            if (buffer[0] != TScalar(0))
-                x(x() / buffer[0]);
-            if (buffer[1] != TScalar(0))
-                y(y() / buffer[1]);
-        }
 
     };
 
